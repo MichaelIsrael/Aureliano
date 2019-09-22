fbdaa9c84cfaf418892c4e494719ca2ea096f6e0
Michael Israel <mich.m.israel@gmail.com>
2019-09-22 20:24:31 +0200

    Major refactoring for configuring parameters.

--- Aureliano/Commands/CommandBase.py
+++ Aureliano/Commands/CommandBase.py
@@ -13,69 +13,113 @@ class Helper:
     def __init__(self, Commando, Help=None):
         self._info = {}
         try:
-            self._info["help"] = Commando.getBriefHelp(Commando)
             self._info["name"] = Commando.__name__[3:]
         except AttributeError:
-            self._info["name"] = Commando
-            """
             try:
-                self._info["help"] = Help[0]
-            """
-            if Help is None:
-                #TODO: Extract class and help
-                pass
-            else:
+                Help, Params = Help
+            except ValueError:
+                Params = ""
+            finally:
+                self._info["name"] = "[{}] {}".format("|".join(Commando), Params).strip()
+                if len(self._info["name"]) >= 20:
+                    self._info["help"] = "\n" + " "*20 + Help
+                else:
+                    self._info["help"] = Help
+        else:
+            try:
+                self._info["help"] = Commando.getBriefHelp(Commando)
+            except AttributeError:
                 self._info["help"] = Help
 
     def __str__(self):
         return repr(self)
 
     def __repr__(self):
-        return "{name: <20}{help}".format(**self._info)
+        """
+        if len(self._info["name"]) >= 20:
+            return "{name:}\n{help}".format(**self._info)
+        else:
+        """
+        if True:
+            return "{name: <20}{help}".format(**self._info)
+
+
+class ParametersGroup(object):
+    def __init__(self, Name, Help, Greedy=False):
+        self.Name = Name
+        self.Help = Help
+        self._Greedy = Greedy
+        self._ready = False
+        self._OptionalParameters = False
+
+        self._Regex = "(?P<FullCommand>(?P<Command>{})".format(Name)
+
+    def addParameter(self, Name, Regex, Help="", Optional=False):
+        Suffix = r''
+        Spacer = r'\s+'
+        if(Optional):
+            self._OptionalParameters = True
+        else:
+            if Optional is False and self._OptionalParameters:
+                raise NotImplementedError
+
+        if self._OptionalParameters:
+            Spacer = r'\s*'
+            Suffix = r'?'
+
+        self._Regex += Spacer + r'(?P<{}>\b'.format(Name) + Regex + r'\b)' + Suffix
+
+    def getRegex(self):
+        assert self._ready, "Internal error: ParametersGroup '{}' was not finalized!".format(self.Name)
+        try:
+            return self._Regex
+        except AttributeError:
+            return None
+
+    def finalize(self):
+        if self._Greedy:
+            self._Regex += "(\s*(?P<Extra>\S.*))?"
+        self._Regex += ")$"
+        self._ready = True
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return self.getRegex()
+
 
 class ParametersBuilder(object):
-    def __init__(self):
-        pass
-
-
-    def addMain(self, Name, Regex, Optional=False):
-        if(Optional):
-            raise NotImplementedError
-
-        try:
-            self.__MainRegex = self.__MainRegex + r'\s+' + "(?P<{}>".format(Name) + Regex + ")"
-        except AttributeError:
-            self.__MainRegex = "(?P<{}>".format(Name) + Regex + ")"
-
-
-    def addExtended(self, Name, Regex, Optional=False):
-        if(Optional):
-            raise NotImplementedError
-
-        try:
-            self.__MultiRegex = self.__MultiRegex + r'\s+' + "(?P<{}>".format(Name) + Regex + ")"
-            self.__GroupedMultiRegex = self.__GroupedMultiRegex + r'\s+' + Regex
-        except AttributeError:
-            self.__MultiRegex = "(?P<{}>".format(Name) + Regex + ")"
-            self.__GroupedMultiRegex = "(?P<Multi>" + Regex
+    def __init__(self, name):
+        self._OptionalMainParameter = False
+        self.MainGroup = ParametersGroup(name.lower(), "TODO", True)
+        self.ExtendedGroups = []
 
 
     def getMainRegex(self):
-        return self.__MainRegex
+        return self.MainGroup.getRegex()
 
 
     def getExtendedRegex(self):
-        try:
-            return self.__GroupedMultiRegex, self.__MultiRegex
-        except AttributeError:
-            return None, None
+        Regex = "({})".format('|'.join(self))
+        return Regex
+        
+
+
+    def newExtendedGroup(self, *args, **kwargs):
+        #newGroup = ParametersGroup(Name, Help)
+        newGroup = ParametersGroup(*args, **kwargs)
+        self.ExtendedGroups.append(newGroup)
+        return newGroup
 
 
     def finalize(self):
-        try:
-            self.__GroupedMultiRegex = self.__GroupedMultiRegex + ")"
-        except AttributeError:
-            pass
+        self.MainGroup.finalize()
+        for Group in self.ExtendedGroups:
+            Group.finalize()
+
+    def __iter__(self):
+        return iter([str(grp) for grp in self.ExtendedGroups])
 
 
 ###########################
@@ -88,15 +132,18 @@ class CommandBase(object):
         assert self.__class__.__name__[:3] == "Cmd", "Invalid inheritance! Class name should start with Cmd"
         self._Aureliano = Aureliano
         self.name = self.__class__.__name__[3:]
-        self.__createRegexes()
-        self.__parseArguments(arguments)
+        try:
+            self.__createRegexes()
+            self.__parseArguments(arguments)
+        except:
+            raise Exception("Implementation error!")
 
 
     def __createRegexes(self):
-        self.Parameters = ParametersBuilder()
+        self.Parameters = ParametersBuilder(self.name)
 
-        self.addMainParameter = self.Parameters.addMain
-        self.addExtendedParameter = self.Parameters.addExtended
+        self.addMainParameter = self.Parameters.MainGroup.addParameter
+        self.createExtendedParametersGroup = self.Parameters.newExtendedGroup
 
         self.registerParameters()
 
@@ -108,74 +155,59 @@ class CommandBase(object):
 
 
     def __parseArguments(self, arguments):
-        ## Automatically build Regex
-        #SingleRegex, GroupedMultiRegex, MultiRegex = self.__buildArgsRegex()
-        SingleRegex = self.Parameters.getMainRegex()
-        GroupedMultiRegex, MultiRegex = self.Parameters.getExtendedRegex()
+        ## get Regexes
+        MainRegex = self.Parameters.getMainRegex()
+        ExtendedRegex = self.Parameters.getExtendedRegex()
 
-        ## One-liner
-        if type(arguments) is str:
-            ## Build full Regex
-            try:
-                CmdRegex = r'\s*' + SingleRegex + r'\s+' + GroupedMultiRegex + r'\s*'
-            except TypeError: #The command has only SingleRegex
-                CmdRegex = r'\s*' + SingleRegex + r'\s*'
-
-            ## Check syntax
-            try:
-                self.Args = re.match(CmdRegex, arguments).groupdict()
-            except AttributeError:
+        ## Check syntax
+        try:
+            ## One-liner
+            self.Args = re.match(MainRegex, arguments).groupdict()
+            ExtendedArgs = [self.Args.pop("Extra")]
+        except AttributeError:
                 raise BadCommandSyntaxError(self._Aureliano, self.name, arguments) from None
-
-            try:
-                MultiParameters = self.Args.pop("Multi")
-                MultiArgs = re.match(MultiRegex, MultiParameters).groupdict()
-                self.Args["Multi"] = [MultiArgs]
-            except AttributeError:
-                raise BadCommandSyntaxError(self._Aureliano, self.name, arguments) from None
-        else: ## Multi-line command.
-            ## Check if command is multi-lineable
-            if MultiRegex is None:
-                raise BadSyntaxError(self._Aureliano, "Not a multi-lineable command!") from None
-
+        except TypeError: ## Multi-line command.
             ## Parse main command
             try:
-                self.Args = re.match(SingleRegex, arguments[0]).groupdict()
+                self.Args = re.match(MainRegex, arguments[0]).groupdict()
             except AttributeError:
                 raise BadCommandSyntaxError(self._Aureliano, self.name, arguments[0]) from None
 
+            assert self.Args["Extra"] is None, "Unexpected arguments {} in a multiline call.".format(self.Args["Extra"])
+
+            ExtendedArgs = arguments[1:]
+        finally:
+            ExtendedCommands = []
             ## Parse multi-line commands.
-            MultiArgs = []
-            for arg in arguments[1:]:
-                try:
-                    MultiArgs.append(re.match(MultiRegex, arg).groupdict())
-                except AttributeError:
+            for arg in ExtendedArgs:
+                for ExtendedRegex in self.Parameters:
+                    try:
+                        ExtendedCommands.append(re.match(ExtendedRegex, arg).groupdict())
+                    except AttributeError:
+                        continue
+                    break
+                else:
                     raise BadCommandSyntaxError(self._Aureliano, self.name, arg) from None
             ## Add multi-line commands.
-            self.Args.update({"Multi": MultiArgs})
+            self.Args.update({"Extended": ExtendedCommands})
+
 
     @staticmethod
-    def __dissectCommand(command):
+    def __dissectCommand(FullCommand):
         try:
-            command = re.split(r'\s+', command, 1)
+            command = re.split(r'\s+', FullCommand, 1)[0]
+            arguments = FullCommand
         except TypeError:
-            mainCmd, param1 = re.split(r'\s+', command[0], 1)
-            params = [param1[:-1]]
-            for paramN in command[1:-1]:
-                params.append(paramN)
-            return mainCmd, params
-        else:
-            try:
-                mainCmd, parameters = command
-            except ValueError:
-                return command[0], ''
-            else:
-                return mainCmd, parameters
+            command = re.split(r'\s+', FullCommand[0], 1)[0]
+            arguments = [FullCommand[0][:-1]]
+            arguments.extend(FullCommand[1:-1])
+        finally:
+            return command, arguments
 
     @classmethod
     def create(Class, Aureliano, command):
-        mainCommand, parameters = Class.__dissectCommand(command)
-        cmdClassName = "Cmd"+mainCommand.title()
+        CmdName, parameters = Class.__dissectCommand(command)
+        cmdClassName = "Cmd"+CmdName.title()
         for cls in Class.__subclasses__():
             if cmdClassName == cls.__name__:
                 cmdClass = cls
